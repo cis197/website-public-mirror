3c313d7d64f143290563bc78a5dfbb8a9156ad5f
cacabo <cameroncabo@gmail.com>
2020-07-04 12:25:20 -0700

    Add scripts, license, readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# CIS 197 Website
+
+This is a public, open source mirror of the CIS 197 website repository. The 197 site is private since it has scripts, solutions, and build files which are needed to release HW assignments and to assist in deploying the site statically to Penn's servers.
+
+For those with access to the private site, to update this repo, clone it and then run:
+
+```bash
+source ./mirror.sh
+```
---
+++ mirror.sh
@@ -0,0 +1,27 @@
+# Script to update the public mirror of the CIS 197 website
+
+echo "Cloning website...\n"
+
+# Remove previous mirror
+rm -rf src
+
+# Clone the private repo
+git clone git@github.com:cis197/website.git
+
+echo "\nRemoving private files...\n"
+
+# Remove files which should not be in the public clone
+rm -rf website/.git
+rm -rf website/scripts
+rm -rf website/public
+rm -rf website/scripts
+rm -rf website/archive
+rm -rf website/homeworks
+
+# Remove license, since there is one at the root of this repo
+rm -rf website/LICENSE
+
+# Rename file to be src
+mv website src
+
+echo "Done."
